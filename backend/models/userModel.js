a562a62d0a5acc0c1bf32ebf5679ca9483fa8d1e
keith.diazfacebook@gmaikl.com <keithcyrill.diaz@tup.edu.ph>
2024-12-21 16:05:32 +0800

    remove index for email since the email it self has a unique true which automatically create index

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -21,7 +21,6 @@ const userSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 userSchema.index({multiRole: 1});
-userSchema.index({email: 1});
 
 const User = mongoose.model('User', userSchema);
 
--- backend/routes/Product.js
+++ backend/routes/Product.js
@@ -1,14 +1,16 @@
 const { createProduct, getProduct, getProductsById, updateProductById, deleteProductById, getBestSellers, getAllproducts } = require("../controllers/productController");
 
 module.exports = (router) => {
-    
+
+    router.get('/products/getAllProducts', getAllproducts);
+    router.get('/products/getBestSellers', getBestSellers);
     //protected routes
     router.post('/user/products/create', createProduct);
 
     router.get('/user/products/get', getProduct);
     router.get('/user/products/getById/:id', getProductsById);
-    router.get('/user/products/getBestSellers', getBestSellers);
-    router.get('/user/products/getAllProducts', getAllproducts)
+
+   
 
     router.patch('/user/products/update/:id', updateProductById);
 
